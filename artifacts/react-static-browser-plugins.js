88edeecd3adbbad5ab3960d504f56306c890178f
David Drobner <david@drobner.ca>
2020-05-17 01:13:19 -0400

    Initial Commit

---
+++ artifacts/react-static-browser-plugins.js
@@ -0,0 +1,27 @@
+// Imports
+import plugin0 from 'C:/Users/David/ics4uwinteremergingtopics/node_modules/react-static-plugin-reach-router/browser.api.js'
+
+// Plugins
+const plugins = [{
+        location: "C:/Users/David/ics4uwinteremergingtopics/node_modules/react-static-plugin-source-filesystem",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "C:/Users/David/ics4uwinteremergingtopics/node_modules/react-static-plugin-reach-router",
+        plugins: [],
+        hooks: plugin0({})
+      },
+{
+        location: "C:/Users/David/ics4uwinteremergingtopics/node_modules/react-static-plugin-sitemap/dist",
+        plugins: [],
+        hooks: {}
+      },
+{
+        location: "C:/Users/David/ics4uwinteremergingtopics",
+        plugins: [],
+        hooks: {}
+      }]
+
+// Export em!
+export default plugins
---
+++ artifacts/react-static-templates.js
@@ -0,0 +1,14 @@
+
+
+  
+// Template Map
+export default {
+  'C:/Users/David/ics4uwinteremergingtopics/src/pages/404.js': require('C:/Users/David/ics4uwinteremergingtopics/src/pages/404.js').default,
+'C:/Users/David/ics4uwinteremergingtopics/src/pages/about.js': require('C:/Users/David/ics4uwinteremergingtopics/src/pages/about.js').default,
+'C:/Users/David/ics4uwinteremergingtopics/src/pages/blog.js': require('C:/Users/David/ics4uwinteremergingtopics/src/pages/blog.js').default,
+'C:/Users/David/ics4uwinteremergingtopics/src/pages/index.js': require('C:/Users/David/ics4uwinteremergingtopics/src/pages/index.js').default,
+'C:/Users/David/ics4uwinteremergingtopics/src/containers/Post': require('C:/Users/David/ics4uwinteremergingtopics/src/containers/Post').default
+}
+
+export const notFoundTemplate = 'C:/Users/David/ics4uwinteremergingtopics/src/pages/404.js'
+
