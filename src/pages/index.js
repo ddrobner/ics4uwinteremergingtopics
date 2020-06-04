b9976be69970431bcaf7c1f2d1351d7c25a9f1ec
David Drobner <david@drobner.ca>
2020-06-04 13:42:32 -0400

    aaaa

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,5 +6,12 @@ export default () => (
       <h1> Machine Learning </h1>
       <h2> By: David Drobner </h2>
     </div>
+    <div style={{marginLeft: "25%", marginRight: "25%"}}>
+      <p>
+        This is my site about AI (with a focus on Machine Learning) for the ICS4U assignment "ics4uwinteremergingtopics"
+        Here, I will be discussing what machine learning is, how it works, it's applications and real world examples of
+        those and the history of machine learning.
+      </p>
+    </div>
   </div>
 )
